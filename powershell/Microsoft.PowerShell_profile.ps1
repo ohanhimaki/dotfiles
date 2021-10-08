fc50d9fcdb8504df8af8f9b6c4102907c2c2c739
Olli Hanhimäki <ohanhimaki@gmail.com>
2021-10-08 22:28:56 +0300

    Rakennetta muokattu

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+Import-Module posh-git
+Import-Module ZLocation
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
