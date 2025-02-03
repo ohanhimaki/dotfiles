98e35bdb0abb19b272c2bc74b6f804495e5e1a07
Olli Hanhimäki <olli.hanhimaki@oiwasolutions.fi>
2025-02-03 08:14:05 +0200

    Talteen excali scripti yms

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -94,3 +94,17 @@ Set-PSReadLineKeyHandler -Key Ctrl+Shift+s `
         }
         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
     }
+
+
+function ned {
+  $NED_FILE = "c:\projects\Notes\Drawings\$($args -join '-').excalidraw.png"
+
+  if (Test-Path -Path $NED_FILE) {
+    echo "File already exists: $NED_FILE"
+  } else {
+    echo "Created $NED_FILE"
+    New-Item -Path $NED_FILE -ItemType File -Force
+    code $NED_FILE
+  }
+}
+
