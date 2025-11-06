dfc748df9f76ce3c552729817b4043387e8c63df
Olli Hanhimäki <ohanhimaki@gmail.fi>
2025-11-06 20:32:46 +0200

    muutamat awesome conffit

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -92,6 +92,7 @@ myawesomemenu = {
    { "edit config", editor_cmd .. " " .. awesome.conffile },
    { "restart", awesome.restart },
    { "quit", function() awesome.quit() end },
+   { "shutdown", function() awful.spawn("shutdown -h now") end },
 }
 
 local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
@@ -522,6 +523,9 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+    -- default brawe to screen 1 tag 3
+    { rule = { class = "Brave-browser" },
+      properties = { screen = 1, tag = "3" } },
 }
 -- }}}
 
@@ -581,9 +585,9 @@ client.connect_signal("request::titlebars", function(c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = false})
-end)
+-- client.connect_signal("mouse::enter", function(c)
+--     c:emit_signa-- l("request::activate", "mouse_enter", {raise = false})
+-- end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
