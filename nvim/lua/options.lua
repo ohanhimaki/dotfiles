28d1ba0e5931a7a76643eba4897d1c6fabc66f6b
Olli Hanhimäki <ohanhimaki@gmail.fi>
2025-11-11 17:31:14 +0200

    nvim fixit läppärille

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,16 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+
+vim.api.nvim_create_autocmd("VimEnter", {
+callback = function()
+io.stdout:write("\027[>1u")
+end,
+})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+callback = function()
+io.stdout:write("\027[<1u")
+end,
+})
