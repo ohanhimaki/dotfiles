ba900cc0a530d08c0f3ecf91ca4d02d90f7dabf9
Olli Hanhimäki <ohanhimaki@gmail.fi>
2025-11-11 17:45:57 +0200

    backspace conffailua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -10,5 +10,15 @@ map("i", "jk", "<ESC>")
 
 map("n", "<leader>cc", "<cmd>CopilotChat<cr>", { desc = "Run CopilotChat" })
 
-
+-- Terminal key mappings for Kitty
 vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-BS>', '<C-W>', { noremap = true, silent = true })
+
+-- Insert mode: delete word backward
+map("i", "<C-BS>", "<C-W>", { desc = "Delete word backward" })
+
+-- Normal mode: delete word backward (like 'db' command)
+map("n", "<C-BS>", "db", { desc = "Delete word backward in normal mode" })
+
+-- Command mode: delete word backward
+map("c", "<C-BS>", "<C-W>", { desc = "Delete word backward in command mode" })
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,10 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+
+-- Terminal key handling fixes
+o.ttimeout = true
+o.ttimeoutlen = 50
+o.timeoutlen = 500
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
-
+-- Enhanced keyboard protocol for better key handling
 vim.api.nvim_create_autocmd("VimEnter", {
 callback = function()
 io.stdout:write("\027[>1u")
