85ef68162d49d32a2c21002f2c48ae10f365558d
Olli Hanhimäki <ohanhimaki@gmail.fi>
2025-11-23 08:19:31 +0200

    pientä conffailua

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -1,3 +1,4 @@
+-- vim: filetype=lua
 -- Simplified rc.lua using modular structure
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
