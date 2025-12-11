3fc3c8cb2945deada588e420d9846d49563f0b9f
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2025-12-11 11:54:03 +0200

    talteen konffeja

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -347,5 +347,21 @@ return {
   },
       })
     end,
+},
+{
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+    config = function(_, opts)
+      require("oil").setup(opts)
+      --- Keymaps
+      vim.keymap.set("n", "<leader>o", require("oil").open, { desc = "Open Oil" })
+    end,
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
 }
 }
