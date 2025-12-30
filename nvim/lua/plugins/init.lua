08f4a20cde4aca3ccc692cdd69d02c98a4f3c709
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2025-12-30 15:24:44 +0200

    talteen

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -390,5 +390,34 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+}, {
+
+  'akinsho/toggleterm.nvim',
+    lazy = false,
+  config = function()
+    require('toggleterm').setup()
+    vim.keymap.set({ 'n', 't' }, "<C-'>", function()
+      require('toggleterm').toggle(1, nil, nil, 'float')
+    end, { noremap = true, silent = true, desc = "Toggle terminal float" })
+    vim.keymap.set({ 'n', 't' }, '<Space>!', function()
+      require('toggleterm').toggle(2, nil, nil, 'horizontal')
+    end, { noremap = true, silent = true, desc = "Toggle terminal horizontal" })
+    vim.keymap.set({ 'n', 't' }, '<C-ö>', function()
+      require('toggleterm').toggle_all()
+    end, { noremap = true, silent = true, desc = "Toggle all terminals" })
+
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      -- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd 'autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()'
+  end,
 }
 }
