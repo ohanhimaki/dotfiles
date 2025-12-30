85d91cf0037083d0c39c1814b25cfce9cf3227bf
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2025-12-30 10:55:31 +0200

    talteen

---
+++ nvim/queries/html/injections.scm
@@ -0,0 +1,11 @@
+; Inject TSX/JSX into script tags with type="text/babel"
+((script_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr
+      (quoted_attribute_value (attribute_value) @_type)))
+  (raw_text) @injection.content)
+ (#eq? @_attr "type")
+ (#eq? @_type "text/babel")
+ (#set! injection.language "tsx"))
+
