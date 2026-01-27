81ec0b9050a410f15ab1f74abe027c0360bfef50
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2026-01-27 08:17:47 +0200

    talteen

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -5,7 +5,69 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
       "rcarriga/nvim-dap-ui",
-      "mfussenegger/nvim-dap-python",
+      -- {
+      --   "mfussenegger/nvim-dap-python",
+      --   dependencies = { "mfussenegger/nvim-dap" },
+      --   enabled = true,
+      --   config = function()
+      --     local status, dap = pcall(require, "dap")
+      --     if not status then
+      --       return
+      --     end
+      --     local dappython
+      --     status, dappython = pcall(require, "dap-python")
+      --     if not status then
+      --       return
+      --     end
+      --     dap.configurations.python = {
+      --       {
+      --         type = "python",
+      --         request = "launch",
+      --         name = "Launch file (justMyCode = false)",
+      --         program = "${file}",
+      --         justMyCode = false,
+      --       },
+      --       {
+      --         type = "python",
+      --         request = "launch",
+      --         name = "Launch file with arguments (justMyCode = false)",
+      --         program = "${file}",
+      --         justMyCode = false,
+      --         args = function()
+      --           local args_string = vim.fn.input "Arguments: "
+      --           return vim.split(args_string, " +")
+      --         end,
+      --       },
+      --       {
+      --         type = "python",
+      --         request = "launch",
+      --         name = "Launch file (console = integratedTerminal, justMyCode = false)",
+      --         program = "${file}",
+      --         console = "integratedTerminal",
+      --         justMyCode = false,
+      --         -- pythonPath = opts.pythonPath,
+      --       },
+      --       {
+      --         type = "python",
+      --         request = "launch",
+      --         name = "Launch file with arguments (console = integratedTerminal, justMyCode = false)",
+      --         program = "${file}",
+      --         console = "integratedTerminal",
+      --         justMyCode = false,
+      --         -- pythonPath = opts.pythonPath,
+      --         args = function()
+      --           local args_string = vim.fn.input "Arguments: "
+      --           return vim.split(args_string, " +")
+      --         end,
+      --       },
+      --     }
+      --     dappython.setup("~/.virtualenvs/debugpy/bin/python", {
+      --       include_configs = true,
+      --       console = "integratedTerminal",
+      --       pythonPath = nil,
+      --     })
+      --   end,
+      -- },
     },
     config = function()
       local dap = require "dap"
@@ -119,26 +181,6 @@ return {
           print("Could not evaluate: " .. var)
         end
       end, { noremap = true, silent = true, desc = "Evaluate and copy to clipboard" })
-
-      -- require("dap-python").setup()
-
-      dap.configurations.python = {
-        {
-          type = "python",
-          request = "launch",
-          name = "Launch file",
-          program = "${file}",
-          pythonPath = function()
-            -- Try to find venv python, fallback to system python
-            local venv = os.getenv "VIRTUAL_ENV"
-            if venv then
-              return venv .. "/Scripts/python.exe"
-            end
-            return "python"
-          end,
-        },
-      }
-      require("dap-python").setup()
     end,
     event = "VeryLazy",
   },
