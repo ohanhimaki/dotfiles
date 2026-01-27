8f723dc50f5049af3781772432203794abe4d85d
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2026-01-27 15:25:31 +0200

    talteen

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -50,8 +50,8 @@ return {
 
             sort_lastused = true,
             ignore_current_buffer = true,
-          }
-        }
+          },
+        },
       }
     end,
     config = function(_, opts)
@@ -62,33 +62,29 @@ return {
         telescope.load_extension(ext)
       end
 
-      vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "telescope keymaps" })
-      vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
-      vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
-      vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
-      vim.keymap.set("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
-      vim.keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
-      vim.keymap.set(
-        "n",
-        "<leader>fz",
-        "<cmd>Telescope current_buffer_fuzzy_find<CR>",
-        { desc = "telescope find in current buffer" }
-      )
-      vim.keymap.set("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
-      vim.keymap.set("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
-      vim.keymap.set("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
+      local map = function(lhs, rhs, desc)
+        vim.keymap.set("n", lhs, rhs, { desc = "telescope " .. desc })
+      end
+      map("<leader>fk", "<cmd>Telescope keymaps<cr>", " keymaps")
+      map("<leader>fw", "<cmd>Telescope live_grep<CR>", " live grep")
+      map("<leader>fb", "<cmd>Telescope buffers<CR>", " find buffers")
+      map("<leader>fh", "<cmd>Telescope help_tags<CR>", " help page")
+      map("<leader>fgg", "<cmd>Telescope git_status<CR>", " git status")
+      map("<leader>fgr", "<cmd>Telescope git_bcommits_range<CR>", " git commits range")
+      map("<leader>fgr", "<cmd>Telescope git_bcommits<CR>", " git commits file")
+      map("<leader>ma", "<cmd>Telescope marks<CR>", " find marks")
+      map("<leader>fo", "<cmd>Telescope oldfiles<CR>", " find oldfiles")
+      map("<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", " find in current buffer")
+      map("<leader>cm", "<cmd>Telescope git_commits<CR>", " git commits")
+      map("<leader>gt", "<cmd>Telescope git_status<CR>", " git status")
+      map("<leader>pt", "<cmd>Telescope terms<CR>", " pick hidden term")
 
-      vim.keymap.set("n", "<leader>th", function()
+      map("<leader>th", function()
         require("nvchad.themes").open()
-      end, { desc = "telescope nvchad themes" })
+      end, " nvchad themes")
 
-      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
-      vim.keymap.set(
-        "n",
-        "<leader>fa",
-        "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-        { desc = "telescope find all files" }
-      )
+      map("<leader>ff", "<cmd>Telescope find_files<cr>", " find files")
+      map("<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>", " find all files")
     end,
   },
 }
