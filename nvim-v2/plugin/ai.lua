97bff7a6bb7f3998555c4ed980eaec14143563e7
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2026-04-03 19:11:16 +0300

    formatointi

--- nvim-v2/plugin/ai.lua
+++ nvim-v2/plugin/ai.lua
@@ -1,25 +1,25 @@
 vim.pack.add({
-  "https://github.com/zbirenbaum/copilot.lua"
+	"https://github.com/zbirenbaum/copilot.lua",
 })
-require("copilot").setup {
-  suggestion = {
-    auto_trigger = true,
-    keymap = {
-      accept = "<C-w>",
-      dismiss = "<C-d>",
-    },
-  },
-  panel = {
-    enabled = true,
-    auto_refresh = true,
-  },
-  nes = {
-    enabled = false, -- requires copilot-lsp as a dependency
-    auto_trigger = false,
-    keymap = {
-      accept_and_goto = false,
-      accept = false,
-      dismiss = false,
-    },
-  },
-}
+require("copilot").setup({
+	suggestion = {
+		auto_trigger = true,
+		keymap = {
+			accept = "<C-w>",
+			dismiss = "<C-d>",
+		},
+	},
+	panel = {
+		enabled = true,
+		auto_refresh = true,
+	},
+	nes = {
+		enabled = false, -- requires copilot-lsp as a dependency
+		auto_trigger = false,
+		keymap = {
+			accept_and_goto = false,
+			accept = false,
+			dismiss = false,
+		},
+	},
+})
