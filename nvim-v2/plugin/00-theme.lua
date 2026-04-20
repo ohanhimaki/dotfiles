58efa6f2e2c85e5f75048d7136842836e67e68ea
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2026-04-20 16:06:02 +0300

    transparent_mode pois

--- nvim-v2/plugin/00-theme.lua
+++ nvim-v2/plugin/00-theme.lua
@@ -21,7 +21,7 @@ require("gruvbox").setup({
 	-- palette_overrides = {},
 	-- overrides = {},
 	-- dim_inactive = false,
-	transparent_mode = true,
+	transparent_mode = false,
 })
 
 vim.cmd("colorscheme gruvbox")
