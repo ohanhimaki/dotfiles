ab10ebcc9d9a535e76213c165760e19f45d49c6e
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2026-04-26 16:02:05 +0300

    feat: add nes completion mapping

--- nvim/plugin/ai.lua
+++ nvim/plugin/ai.lua
@@ -30,6 +30,17 @@ vim.schedule(function()
 	})
 	local sidekickkeys = {
 		{
+			"<tab>",
+			function()
+				-- if there is a next edit, jump to it, otherwise apply it if any
+				if not require("sidekick").nes_jump_or_apply() then
+					return "<Tab>" -- fallback to normal tab
+				end
+			end,
+			expr = true,
+			desc = "Goto/Apply Next Edit Suggestion",
+		},
+		{
 			"<c-n>",
 			function()
 				require("sidekick.cli").focus()
